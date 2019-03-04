eada7f586a383b64f9d5fd6f5d9bd13d95294a22
Paige Wiener <snowplushie@yahoo.com>
2019-03-04 04:39:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(fruits)
   fruits.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  
+def find_the_cheese(contains_cheddar)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
