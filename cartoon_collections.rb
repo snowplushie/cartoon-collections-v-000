616d1fbdd544952443d0206ee26986ffc94c471f
Paige Wiener <snowplushie@yahoo.com>
2019-03-04 04:21:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 def roll_call_dwarves(names)
-  names.each_with_index
+  names.each_with_index do |index, names|
+    puts "#{index}. #{names}"
 end
 
 def summon_captain_planet# code an argument here
