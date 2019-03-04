3f5c28cc2cd074b2827ba108e4d94a6fec660bbb
Paige Wiener <snowplushie@yahoo.com>
2019-03-04 04:30:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.join
-  array.collect do |
+  planeteer_calls.join("!")
 end
 
 def long_planeteer_calls# code an argument here
