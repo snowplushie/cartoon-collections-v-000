a2569782579d01bb1e135c8e8efb964e92e02c88
Paige Wiener <snowplushie@yahoo.com>
2019-03-04 04:27:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  array = veggies.split
+  array.collect do |
 end
 
 def long_planeteer_calls# code an argument here
