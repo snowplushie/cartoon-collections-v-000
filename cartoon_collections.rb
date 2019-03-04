4ebb15b6712cf96eb9a759c9354cc1efff319e75
Paige Wiener <snowplushie@yahoo.com>
2019-03-04 04:37:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def long_planeteer_calls(fruits)
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
