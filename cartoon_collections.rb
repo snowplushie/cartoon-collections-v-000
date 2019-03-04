ba2ba810c1baad735d2f8611aeed0ee3366cfcf9
Paige Wiener <snowplushie@yahoo.com>
2019-03-04 04:40:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 def find_the_cheese(contains_cheddar)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    if contains_cheddar
+    return cheese if contains_cheddar.include?(cheese)
 end
